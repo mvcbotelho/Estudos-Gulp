9338b0eb585c93da5c412a00f500feafdff1d583
marcusbotelho <marcus.itec@gmail.com>
2017-12-15 23:28:20 -0300

    Minificação do JS e do CSS. Implemntação do browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
-var gulp = require('gulp')
-  ,imagemin = require('gulp-imagemin')
-  ,clean = require('gulp-clean');
+var gulp = require('gulp'),
+	imagemin = require('gulp-imagemin'),
+	clean = require('gulp-clean'),
+	concat = require('gulp-concat'),
+	htmlReplace = require('gulp-html-replace'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+	cssmin = require('gulp-cssmin'),
+    browserSync = require('browser-sync');
 
+gulp.task('default', ['copy'], function () {
+
+	gulp.start('build-img', 'usemin');
+});
 
 gulp.task('copy', ['clean'], function() {
 	return gulp.src('src/**/*')
@@ -13,11 +23,27 @@ gulp.task('clean', function() {
 		.pipe(clean());
 });
 
-gulp.task('build-img', ['copy'], function() {
+gulp.task('build-img', function() {
 
   gulp.src('dist/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
 });
 
+gulp.task('usemin', function () {
+	return gulp.src('dist/**/*.html')
+		.pipe(usemin({
+			js: [uglify],
+			css: [cssmin]
+		}))
+		.pipe(gulp.dest('dist'));
+});
 
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    });
+    gulp.watch('src/**/*').on('change', browserSync.reload);
+});
