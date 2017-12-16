9968bc34c7ea487af163c48a2d517729e30ee94d
marcusbotelho <marcus.itec@gmail.com>
2017-12-15 22:35:19 -0300

    Nova versão.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
-var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin'),
-    clean = require('gulp-clean');
+var gulp = require('gulp')
+  ,imagemin = require('gulp-imagemin')
+  ,clean = require('gulp-clean');
 
-gulp.task('copy', ['clean'], function () {
-    return gulp.src('src/**/*')
-        .pipe(gulp.dest('dist'));
+
+gulp.task('copy', ['clean'], function() {
+	return gulp.src('src/**/*')
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', function () {
-    return gulp.src('dist')
-        .pipe(clean());
-
+gulp.task('clean', function() {
+	return gulp.src('dist')
+		.pipe(clean());
 });
 
-gulp.task('build-img', ['copy'], function () {
-    gulp.src('dist/img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'));
+gulp.task('build-img', ['copy'], function() {
 
+  gulp.src('dist/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
 });
 
 
